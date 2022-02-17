918feaa46ec115e27bab70d9990df89d2973f3e7
Stalkover <armankarimov24@gmail.com>
2022-02-17 12:40:53 +0600

    First commit

---
+++ test.js
@@ -0,0 +1,16 @@
+function fn()
+{
+var audio = new Audio('bystro-palcem-dollary.mp3');
+audio.play();
+    if (rd1.checked==true)
+        alert("Thank you for buying our : "+rd1.value);
+    else if (rd2.checked==true)
+        alert("Thank you for buying our: "+rd2.value);
+    else if (rd3.checked==true)
+        alert("Thank you for buying our: "+rd3.value);
+    else if (rd4.checked==true)
+        alert("Thank you for buying our: "+rd4.value);
+    else
+        alert("Nothing selected");
+
+}
